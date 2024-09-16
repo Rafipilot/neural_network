cdcef9dc9bb89abb8b6793105b1101af736a96ef
Rafayel Latif <Rafayel.latif@gmail.com>
2024-09-16 09:31:20 +0100

    added some comments

--- neural_net.py
+++ neural_net.py
@@ -1,31 +1,31 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-class NeuralNetwork():
+class NeuralNetwork(): # neural network class
     
-    def __init__(self, input, output, q):
-        x, input_size = np.shape(input)
-        y, output_size = np.shape(output)
+    def __init__(self, input, output, q): # initlizing
+        x, input_size = np.shape(input) # getting the shape of input
+        y, output_size = np.shape(output) # getting shape of output
         
-        np.random.seed(1)
-        # Initialize weights randomly with mean 0
+        np.random.seed(1) # using seed to get the same starting weights each time
+        # Initialize weights randomly 
         self.weights_input_hidden = np.random.randn(input_size, q) * np.sqrt(1. / input_size)
         self.weights_hidden_output = np.random.randn(q, output_size) * np.sqrt(1. / q)
         
         self.bias_hidden = np.zeros(q)  # Bias for hidden layer
         self.bias_output = np.zeros(output_size)  # Bias for output layer
 
-    def sigmoid(self, x):
+    def sigmoid(self, x): # sigmoid activation function
         x = np.clip(x, -500, 500)
         return 1 / (1 + np.exp(-x))
 
-    def sigmoid_derivative(self, x):
+    def sigmoid_derivative(self, x): # sigmoid derivaitve
         return x * (1 - x)
     
-    def relu(self, x):
+    def relu(self, x): # relu activation function to get rid of vanishing gradient problem
         return np.maximum(0, x)
     
-    def relu_derivative(self, x):
+    def relu_derivative(self, x): # relu derivative
         return np.where(x > 0, 1, 0)
 
     def train(self, training_inputs, training_outputs, iterations, learning_rate):
@@ -38,9 +38,10 @@ class NeuralNetwork():
             
             # Calculate error
             error = training_outputs - final_output
+            print(error)
             errors.append(np.mean(np.abs(error)))
 
-            # Backward pass
+            # Backpropgation to adjust weights and biases based on the errors calculated
             final_output_delta = error * self.sigmoid_derivative(final_output)
             hidden_output_error = final_output_delta.dot(self.weights_hidden_output.T)
             hidden_output_delta = hidden_output_error * self.relu_derivative(hidden_output)
@@ -59,7 +60,7 @@ class NeuralNetwork():
         plt.title('Training Error Over Time')
         plt.show()
 
-    def think(self, inputs):
+    def think(self, inputs): # function to test the neural network
         inputs = inputs.astype(float)
         hidden_output = self.relu(np.dot(inputs, self.weights_input_hidden) + self.bias_hidden)
         final_output = self.sigmoid(np.dot(hidden_output, self.weights_hidden_output) + self.bias_output)
